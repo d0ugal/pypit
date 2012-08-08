888cc4c87d1e5ddeeb5a635eb2214429e677b7b8
Dougal Matthews <dougal85@gmail.com>
2012-08-08 20:37:24 +0100

    Add a command to create the DB

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 
 from flaskext.script import Manager
 
-from app import app
+from app import app, db
 manager = Manager(app)
 
+
+@manager.command
+def createdb():
+    db.create_all()
 
 if __name__ == "__main__":
     manager.run()
