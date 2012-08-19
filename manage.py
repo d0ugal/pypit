f808f9e3176e17fe3ea23e46dca5beddeb63ebce
Dougal Matthews <dougal85@gmail.com>
2012-08-19 18:12:19 +0100

    Updated manage.py

--- manage.py
+++ manage.py
@@ -8,20 +8,20 @@ manager = Manager(app)
 
 @manager.command
 def createdb(drop=False):
+
     if drop:
         db.drop_all()
-    db.create_all()
 
-    from app.package.models import Package
-    print list(Package.query.filter_by(pypi_name="Django"))
+    db.create_all()
 
 
 @manager.command
 def watchreleases():
-    from app.pit.tasks import get_latest_packages
 
-    for package, release in get_latest_packages():
-        print release.package.name, release.version
+    from pit.tasks import get_latest_packages
+
+    for release in get_latest_packages():
+        print release.name, release.version
 
 if __name__ == "__main__":
     manager.run()
