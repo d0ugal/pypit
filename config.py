e994d6b94dce0db187ded8a3ff8058e1f00b1468
Dougal Matthews <dougal85@gmail.com>
2012-08-20 20:35:09 +0100

    Removed the not so secret keys in the config. (They were not being used yet anyway)

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 
 ADMINS = frozenset([''])
-SECRET_KEY = 'PeDFlDSzNKcVrfAvUDCiieFapexGcdFZvoVFMefxhdgCPYymCh'
+SECRET_KEY = ''
 
 SQLALCHEMY_DATABASE_URI = "postgresql://localhost/pypit"
 DATABASE_CONNECT_OPTIONS = {}
@@ -12,7 +12,7 @@ DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 8
 
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = "NosCceUTTIOPjbxLIyoWVFvBWSwLRsBdaGgktQzdoussXArITm"
+CSRF_SESSION_KEY = ""
 
 PYPI_PACKAGES_RSS = 'http://pypi.python.org/pypi?%3Aaction=packages_rss'
 
