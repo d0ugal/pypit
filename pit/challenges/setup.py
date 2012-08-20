6ff6932425f25edea054918fdf7a89f1f9603406
Dougal Matthews <dougal85@gmail.com>
2012-08-20 20:55:23 +0100

    Added an outline of how the challenges should/will work.

--- pit/challenges/setup.py
+++ pit/challenges/setup.py
@@ -1,21 +1,11 @@
-from app.challenges import BaseChallenge
+from app.challenges import PythonChallenge
 
 
-class SetupChallenge(BaseChallenge):
-
-    def setup(self):
-        # 1. create virtualenv template
-        pass
+class SetupChallenge(PythonChallenge):
 
     def run(self):
-        # 1. Download package
-        # 2. clone virtualenv
-        # 2. Install package into virtualenv (use pip as a lib?)
-        # 3. Verify installation worked - try importing? Check for an bin
-        #    files (like django-admin.py)
-        # 4. delete virtualenv
-        pass
-
-    def finish(self):
-        # 1. delete virtualenv template
+        """
+        1. Verify installation worked - try importing? Check for an bin
+           files (like django-admin.py)?
+        """
         pass
---
+++ pit/challenges/sloc.py
@@ -0,0 +1,9 @@
+from app.challenges import SourceChallenge
+
+
+class SetupChallenge(SourceChallenge):
+
+    def run(self):
+        """
+        1. Perform SLOC calcs.
+        """
