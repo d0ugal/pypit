54668e9dab4bc2a2401fbc752aecabe9b775e2e1
Dougal Matthews <dougal85@gmail.com>
2012-08-20 19:46:47 +0100

    Display the total and all new package names.

--- manage.py
+++ manage.py
@@ -20,8 +20,11 @@ def watchreleases():
 
     from pit.tasks import get_latest_packages
 
-    for release in get_latest_packages():
-        print release.name, release.version
+    releases = get_latest_packages()
+    print "Found %s new releases" % len(releases)
+
+    for release in releases:
+        print ' -> %s (%s)' % (release.name, release.version)
 
 if __name__ == "__main__":
     manager.run()
