a62f2d02b31c5f871deb094e9d4a37619b72c5c7
Mathew Taylor <mat@mathewtaylor.co.uk>
2012-08-20 21:59:29 +0100

    add template and static paths to flask

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 def create_app():
-    return Flask("pypit")
+    return Flask("pypit", template_folder='app/templates',
+        static_folder='app/static')
 
 app = create_app()
 app.config.from_pyfile('config.py')
@@ -19,6 +20,9 @@ def not_found(error):
 @app.errorhandler(500)
 def server_error(error):
     return render_template('500.html'), 404
+
+from app import views
+app.register_blueprint(views.mod)
 
 from package.models import *
 from pit.models import *
