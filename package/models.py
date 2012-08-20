13994831f79e89cc2781eb927943694d965861f7
Dougal Matthews <dougal85@gmail.com>
2012-08-20 19:45:47 +0100

    Increased a bunch of lengths that caused errors.

--- package/models.py
+++ package/models.py
@@ -5,43 +5,45 @@ from app import db
 
 class Package(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    pypi_name = db.Column(db.String(30), unique=True)
+    pypi_name = db.Column(db.String(100), unique=True, nullable=False)
+    added = db.Column(db.DateTime, nullable=False)
 
 
 class Release(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    package_id = db.Column(db.Integer, db.ForeignKey('package.id'))
+    package_id = db.Column(db.Integer, db.ForeignKey('package.id'), nullable=False)
     package = db.relation(Package, primaryjoin=(package_id == Package.id),
                  backref=db.backref('packages', order_by=id))
+    added = db.Column(db.DateTime, nullable=False)
 
     # The following fields are copied verbatim from the PyPI JSON package
     #output. For example http://pypi.python.org/pypi/pip/json . We may not want
     #everything, so this can be revisited later. Some of it seems useless.
-    maintainer = db.Column(db.String(30))
+    maintainer = db.Column(db.String(100))
     docs_url = db.Column(db.String(200))
     requires_python = db.Column(db.Boolean)
-    maintainer_email = db.Column(db.String(75))
+    maintainer_email = db.Column(db.String(100))
     cheesecake_code_kwalitee_id = db.Column(db.Integer)
-    keywords = db.Column(db.String(75))
+    keywords = db.Column(db.Text)
     package_url = db.Column(db.String(200))
-    author = db.Column(db.String(75))
-    author_email = db.Column(db.String(75))
+    author = db.Column(db.String(100))
+    author_email = db.Column(db.String(100))
     download_url = db.Column(db.String(200))
-    platform = db.Column(db.String(30))
-    version = db.Column(db.String(10))
+    platform = db.Column(db.String(100))
+    version = db.Column(db.String(20))
     provides = db.Column(ARRAY(db.String(30)))
     cheesecake_documentation_id = db.Column(db.Integer)
     _pypi_hidden = db.Column(db.Boolean)
     description = db.Column(db.Text)
-    release_url = db.Column(db.String(75))
+    release_url = db.Column(db.String(100))
     _pypi_ordering = db.Column(db.Integer)
     classifiers = db.Column(db.Text)
-    name = db.Column(db.String(30))
-    bugtrack_url = db.Column(db.String(75))
+    name = db.Column(db.String(100))
+    bugtrack_url = db.Column(db.String(100))
     license = db.Column(db.Text)
     summary = db.Column(db.Text)
-    home_page = db.Column(db.String(75))
+    home_page = db.Column(db.String(100))
     stable_version = db.Column(db.String(10))
-    requires = db.Column(ARRAY(db.String(30)))
-    cheesecake_installability_id = db.Column(db.String(10))
+    requires = db.Column(ARRAY(db.String(100)))
+    cheesecake_installability_id = db.Column(db.String(20))
