c6cfc3230b45f07acf249a7d2dd2fd2878b83c17
Dougal Matthews <dougal85@gmail.com>
2012-08-26 20:28:15 +0100

    Added hooks for pit.
    Run the gauntlet for a new release when its 
    created using flask-sqlalchemy signal support.

--- pit/models.py
+++ pit/models.py
@@ -1,6 +1,8 @@
-from app import db
+from flask.ext.sqlalchemy import models_committed
 
+from app import app, db
 from package.models import Release
+from pit.tasks import run_the_gauntlet
 
 
 class TestRun(db.Model):
@@ -9,3 +11,11 @@ class TestRun(db.Model):
     release_id = db.Column(db.Integer, db.ForeignKey('release.id'), nullable=False)
     release = db.relation(Release, primaryjoin=(release_id == Release.id),
                  backref=db.backref('test_runs', order_by=id))
+
+
+def on_release_commit(sender, changes):
+    for model, change in changes:
+        if isinstance(model, Release):
+            run_the_gauntlet.delay(model)
+
+models_committed.connect(on_release_commit, sender=app)
--- pit/tasks.py
+++ pit/tasks.py
@@ -1,6 +1,6 @@
 from app.celery import celery
 
 
-@celery.task(name="pit.get_latest_packages", ignore_result=True)
+@celery.task(name="pit.run_the_gauntlet", ignore_result=True)
 def run_the_gauntlet(release):
     pass
